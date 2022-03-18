f26dfcf584593acb00d7713a211f2c176be6ccd3
alexmorinvt <alexmorin@vt.edu>
2022-03-17 23:39:49 -0400

    almost done testing harness. todo: add files to new data folder

--- Harness/test_harness.py
+++ Harness/test_harness.py
@@ -1,16 +1,26 @@
-from turtle import xcor
+from turtle import shape, xcor
 import pandas as pd
 import numpy as np
 import argparse
+from sklearn import metrics
+import argparse
+from sys import __stdout__, exit
+from os import devnull
 
 #MAPS tests to file name and location
 file_map = {"test":"../TestData/antibiotic_activity.csv"}
+rng = None
 
-def main():
-    return 0
+# Disable
+def blockPrint():
+    stdout = open(devnull, 'w')
 
-#Returns a map with each test_name : (train_data, test_x, test_y) after preparing data. Keeps the columns NAME, SMILES, SELFIES, TOXICITY, then everything after.
-def load_dfs(tests, light, split):
+# Restore
+def enablePrint():
+    stdout = __stdout__
+
+#Returns a map with each test_name : (train_data, test_data, test_y) after preparing data. Keeps the columns NAME, SMILES, SELFIES, TOXICITY, then everything after.
+def load_dfs(tests, light, split, balanced):
     map = {}
 
     x_cols = ['NAME', 'SMILES', 'SELFIES']
@@ -19,8 +29,15 @@ def load_dfs(tests, light, split):
 
     #Make sure there are no extraneous columns in df TODO: Make more flexible
     for test_name in tests:
-        df = pd.read_csv(file_map[test_name])
+        df = pd.read_csv(file_map[test_name]).sample(frac=1)
         
+        #Produces a balanced df based on TOXICITY
+        if balanced:
+            df = df.groupby('TOXICITY')
+            df = df.apply(lambda x: x.sample(df.size().min()).reset_index(drop=True))
+            df = df.sample(frac=1)
+
+        #Splitting data
         train_length = int(df.shape[0] * split)
         train = df.iloc[:train_length,:]
         test = df.iloc[train_length:,:]
@@ -31,27 +48,31 @@ def load_dfs(tests, light, split):
         test_x = test[x_cols]
         test_y = test[tox_col]
 
+        #Pass on descriptors?
         if light:
             map[test_name] = {"train":(train_x, train_y), "test":(test_x), "harness":(test_y)}
         else:
-            train_descriptors = df.loc[:, ~df.columns.isin(all_x)]
-            map[test_name] = {"train":(train_x, train_y, train_descriptors), "test":(test_x), "harness":(test_y)}
+            train_descriptors = train.loc[:, ~train.columns.isin(all_x)]
+            test_descriptors = test.loc[:, ~test.columns.isin(all_x)]
+            map[test_name] = {"train":(train_x, train_y, train_descriptors), "test":(test_x, test_descriptors), "harness":(test_y["TOXICITY"].values)}
         
     return map
 
-if __name__ == "__main__":
+def main():
 
-    import argparse
-    # import sys
+    # Import your file as such, add path if necessary
+
     # sys.path.append("./mnist_test.py")
 
-    #Import your file as such, add path if necessary
+    # Class needs two methods, train and test
+    # Train is of the form train( (x, y, descriptors) ) or ( (x, y) )
+    # Test is of the form test( (x, descriptors) )
+    # For both, descriptors is optional in the passed tuple. Look for the -light argument
     from unit_testing import Tester
+    model = Tester()
 
     #train method must be of the form train([chemical name, SMILES, SELFIES, toxicity], [descriptors])
     #test method must be of the form test([chemical name, SMILES, SELFIES], [descriptors])
-
-    model = Tester()
 
     descriptors = True 
 
@@ -61,30 +82,87 @@ if __name__ == "__main__":
     parser.add_argument('-t', nargs='*', default = ["p53"], help='Specify the tests to be ran')
     parser.add_argument('-v', default = False, action='store_true', help='If specified, print out info on training data')
     parser.add_argument('-save', default = False, action  = 'store_true', help='Saves the each output data to a file named: \'test\' + time.csv')
+    parser.add_argument('-n', default = 1, help='Number of repititions for each model')
     parser.add_argument('-split', default = 0.75, help='Percentage to be used as train/test split')
-
-    # -light sends over [name, SMILES, SELFIES, [empty], toxicity]
+    parser.add_argument('-seed', default=42, help='Seed for randomization')
     parser.add_argument('-light', default = False, action='store_true', help='Does not send descriptors to train to save space')
     parser.add_argument('-test', dest='test_run', action='store_true', help='Only for testing the harness')
+    parser.add_argument('-m', nargs='*', default = ["report", "confusion_matrix"], help='Specify the metrics to be ran')
 
     args = parser.parse_args()
 
+    np.random.seed(args.seed) #Seed Done
+    #Use the random number generator if you'd like
+    rng = np.random.default_rng(args.seed)
+
     #Verbose setting
-    verbose = args.v
-    balanced = args.b
-    all_tests = args.a
-    no_descriptors = args.light
-    save = args.save
+    verbose = args.v #Done
+    balanced = args.b  #Done
+    all_tests = args.a #Done
+    no_descriptors = args.light #Done
+    save = args.save #TODO
+    split = args.split #Done
+    metrs = args.m #Done
 
-    tests = args.t
+    tests = args.t #Done, TODO: Add tests
 
-    if args.test_run:
+    if all_tests:
+        test = file_map.keys()
+        
+    if args.test_run: #Done
         tests = ['test']
 
-    df_map = load_dfs(tests, args.light, args.split)
+    df_map = load_dfs(tests, no_descriptors, split, balanced)
 
+    #Actual loop that loops through the test map
     for test_name, test_map in df_map.items():
 
-        train_y = model.train(test_map["train"])
-        test_y = model.test(test_map["test"])
+        if not verbose:
+            blockPrint()    
+
+        train_y = model.train(test_name, test_map["train"])
+        test_predictions = model.test(test_name, test_map["test"])
+
+        test_y = test_map["harness"]
+
+        if not verbose:
+            enablePrint()
+
+        #Metrics control here
+        #test_predictions should be a vector of the shape (test_length,)
+
+        #I love this metrics package - so simple
+        current_metrics = metrics.classification_report(test_y, test_predictions, target_names = ['non-toxic', 'toxic'], digits = 4)
+        print(current_metrics)
+
+        accuracy = metrics.accuracy_score(test_y, test_predictions)
+        f1score = metrics.f1_score(test_y, test_predictions)
+
+        precision = metrics.precision_score(test_y, test_predictions)
+        mse = metrics.mean_squared_error(test_y, test_predictions)
+        log_l = metrics.log_loss(test_y, test_predictions)
+        confusion_matrix = metrics.confusion_matrix(test_y, test_predictions)
         
+        metrics_map = {'report':current_metrics, 'accuracy':accuracy, 'precision':precision, 'f1':f1score, 'mse':mse,
+            'log_loss':log_l, 'confusion_matrix':confusion_matrix}
+
+        if save:
+            print(test_map['test'][0].shape)
+            test_y = pd.DataFrame(test_y, columns = ['EXPECTED'])
+            test_predictions = pd.DataFrame(test_predictions, columns = ['PREDICTED'])
+            print(test_y.shape)
+
+            df = pd.concat([test_map['test'][0].reset_index(drop=True), test_y.reset_index(drop=True), test_predictions.reset_index(drop=True)], 
+                axis = 1, ignore_index=True)
+            
+            df.columns = list(test_map['test'][0].columns) + ['EXPECTED', 'PREDICTED']
+
+            #TODO: Actually save dis 
+        else:
+            for m in metrs:
+                print(metrics_map[m])
+            
+        
+
+if __name__ == "__main__":
+    main()
--- Harness/unit_testing.py
+++ Harness/unit_testing.py
@@ -1,31 +1,64 @@
 import tensorflow as tf
 import pandas as pd
 import numpy as np
-from scipy import stats
+from tensorflow import keras
 import csv
 
-from keras.models import Sequential
-from keras.layers import Dense
+from tensorflow import nn
+import tensorflow as tf
 
-from rdkit import Chem
-from rdkit.ML.Descriptors import MoleculeDescriptors
-from rdkit.Chem import Descriptors
+import pandas as pd
 
 # df = pd.read_table('../TestData/antibiotic_activity.csv', sep=',')
 
 ayo = -1
 
+class LearningRateReducerCb(tf.keras.callbacks.Callback):
+
+  def on_epoch_end(self, epoch, logs={}):
+    old_lr = self.model.optimizer.lr.read_value()
+    new_lr = old_lr * 0.999
+    print("\nEpoch: {}. Reducing Learning Rate from {} to {}".format(epoch, old_lr, new_lr))
+    self.model.optimizer.lr.assign(new_lr)
+
 class Tester:
     def __init__(self) -> None:
         self.ayo = -1
+        self.model = None
         pass
 
-    def train(self, tup):
-        print(self.ayo)
-        self.ayo = 10
-        names, y, descriptors = tup
+    def train(self, test_name, tup):
+
+        names, y_train, x_train = tup
+
+        self.model = keras.Sequential([
+            keras.layers.Flatten(input_shape=(4,)),
+            keras.layers.Dense(16, activation=nn.relu),
+            keras.layers.Dense(64, activation=nn.relu),
+            keras.layers.Dense(64, activation=nn.relu),
+            keras.layers.Dense(16, activation=nn.relu),
+            keras.layers.Dense(1, activation=nn.sigmoid),
+        ])
+
+        #standard values are lr = 0.001, b1 = 0.9, b2 = 0.999., ep = 1e-07, amsgrad = False
+        opt = tf.keras.optimizers.Adam(
+            learning_rate=0.01, beta_1=0.9, beta_2=0.999, epsilon=1e-07, amsgrad=False,
+            name='Adam',
+        )
+
+        self.model.compile(optimizer=opt,
+                    loss='binary_crossentropy',
+                    metrics=['accuracy'])
+
+        class_weight = {0: 1.,
+                        1: 0.66}
+
+        self.model.fit(x_train, y_train, callbacks=[LearningRateReducerCb()], epochs=10, class_weight = class_weight)
         return 0
 
-    def test(self, tup):
-        print(self.ayo)
-        return -1
+    def test(self, test_name, tup):
+        names, x_test = tup
+
+        predictions = self.model.predict(x_test)
+        predictions = (predictions[:,0] > 0.5).astype(np.int).ravel()
+        return predictions
